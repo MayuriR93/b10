fe6e7be3b164e68484d69912b3a7e3dc3afee1f8
MayuriR93 <mayurirane36@gmail.com>
2024-02-11 22:27:49 +0530

    first commit

---
+++ app1/db_shell.py
@@ -0,0 +1,18 @@
+from app1.models import Employee
+
+# exec(open(r'D:\Python Practice\Django_Practice\first_project\app1\db_shell.py').read())
+
+# CRUD --- read
+# all_emps = Employee.objects.all()
+# # print(all_emps)
+# for emp in all_emps:
+#     print(emp.id)
+
+# Single read karnyasathi
+# jr employee present nasel tr error yeil tyamule risky code try block madhe takaycha aani error msg. dyaycha. 
+# try:
+#     emp = Employee.objects.get(first_name='Pooja')
+#     print(emp)
+# except Employee.DoesNotExist as msg:
+#     print("Employee does not exist", msg)
+
